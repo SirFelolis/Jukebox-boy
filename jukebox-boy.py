52ea6ac51a42e2c9e34a02081debe7b94f3dc0e4
Oliver Thunæs <Felolis30@gmail.com>
2016-08-29 21:19:29 +0200

    Started working on musique class, also added some dependencies to the repo

--- jukebox-boy.py
+++ jukebox-boy.py
@@ -3,12 +3,27 @@ import discord
 from discord.ext import commands
 import tokens
 
-bot = commands.Bot(command_prefix='!', description="Jukebox boi!!!!!!!!!!!!!!")
-
 if not discord.opus.is_loaded():
 	discord.opus.load_opus()
 
-print('Logging in...')
+bot = commands.Bot(command_prefix="!")
+
+#class Musique:
+
+#	def __init__self(self, bot):
+#		self.bot = bot
+#		self.players = {}
+#		self.default_vol = 100
+
+#	@commands.group(pass_context=True)
+#	async def musique(self, ctx):
+#		if ctx.invoked_subcommand is None:
+#			self.bot.say("Sorry my dude, it's not wednesay")
+
+#	@musique.command(name="join", pass_context=True)
+#	async def join_vc_and_play_stream(self, ctx, *, channel: discord.Channel = None):
+#		try:
+#			voice_client = await self.bot.join_voice_channel(channel)
 
 @bot.command(pass_context=True)
 async def play(ctx, url):
@@ -20,12 +35,13 @@ async def play(ctx, url):
 async def join(ctx):
 	voice = await bot.join_voice_channel(ctx.message.author.voice.voice_channel)
 
+print('Logging in...')
+
 @bot.event
 async def on_ready():
-    print('------------------')
-    print('Logged in as ' + bot.user.name)
-    print('ID: ' + bot.user.id)
-    print('------------------')
-
+  print('------------------')
+  print('Logged in as ' + bot.user.name)
+  print('ID: ' + bot.user.id)
+  print('------------------')
 
 bot.run(tokens.key)
